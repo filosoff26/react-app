327f9b7bc14417e22b1af453a243ea64ca0049a9
Your Name <you@example.com>
2021-02-23 19:41:37 +0700

    create sample react app

---
+++ src/Components/Main.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div className="container">
+            <h1
+                style={{
+                    textAlign: "center",
+                    marginTop: "2rem",
+                    marginBottom: "1rem",
+                }}
+            >
+                Hello world!
+            </h1>
+            <hr />
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/Components/NavBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+const NavBar = () => {
+    return (
+        <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="/">Todo app</Navbar.Brand>
+            <Nav className="ml-auto">
+                <Nav.Link href="/about">О приложении</Nav.Link>
+                <Nav.Link href="/features">Особенности</Nav.Link>
+                <Nav.Link href="/price">Стоимость</Nav.Link>
+            </Nav>
+        </Navbar>
+    );
+};
+
+export default NavBar;
